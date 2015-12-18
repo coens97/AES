a8cc8d63d49f37c599d8d7cfcaf73e1fed6efcae
Coen Stange <coen.stange@hotmail.com>
2015-12-18 14:05:11 +0100

    + Added begin to cipher but not working

---
+++ Aes/AesCipher.cs
@@ -0,0 +1,45 @@
+﻿namespace Aes
+{
+    public class AesCipher
+    {
+        public static State Cipher(Key key, State state)
+        {
+            state = state
+                .AddRoundKey(key, 0);
+
+            for (var i = 1; i < 10; i++)
+            {
+                state = state
+                    .SubBytes()
+                    .ShiftRows()
+                    .MixColumns()
+                    .AddRoundKey(key, i);
+            }
+            state = state
+                .SubBytes()
+                .ShiftRows()
+                .AddRoundKey(key, 10);
+            return state;
+        }
+
+        public static State CipherInv(Key key, State state)
+        {
+            state = state
+                .AddRoundKey(key, 10);
+
+            for (var i = 10; i  > 0; i--)
+            {
+                state = state
+                    .ShiftRowsInv()
+                    .SubBytesInv()
+                    .AddRoundKey(key, i)
+                    .MixColumnsInv();
+            }
+            state = state
+                .ShiftRowsInv()
+                .SubBytesInv()
+                .AddRoundKey(key, 0);
+            return state;
+        }
+    }
+}
---
+++ IntegrationTestAes/IntegrationTestAes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Aes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntegrationTestAes
+{
+    [TestClass]
+    public class IntegrationTestAes
+    {
+        // values from http://www.inconteam.com/software-development/41-encryption/55-aes-test-vectors#aes-ecb-128
+        private readonly byte[] _bkey =
+        {
+            0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a
+        };
+
+        private readonly byte[] _input =
+        {
+                0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a
+            };
+
+        private readonly byte[] _expectedResult =
+        {
+                0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40,0x9f, 0x96, 0xe9,0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a
+        };
+
+        [TestMethod]
+        public void TestAesCipher()
+        {
+            var key = new Key(_bkey);
+
+            var inputState = new State(_input);
+            var expectedState = new State(_expectedResult);
+
+            var cipher = AesCipher.Cipher(key, inputState);
+
+            Assert.AreEqual(cipher.ToString(), expectedState.ToString());
+        }
+
+        [TestMethod]
+        public void TestAesCipherInv()
+        {
+            var key = new Key(_bkey);
+
+            var expectedNormal = new State(_input);
+            var expectedState = new State(_expectedResult);
+
+            var normal = AesCipher.CipherInv(key, expectedState);
+
+            Assert.AreEqual(normal.ToString(), expectedNormal.ToString());
+        }
+
+        [TestMethod]
+        public void TestAesCipherAndBack()
+        {
+            var key = new Key(_bkey);
+
+            var inputState = new State(_input);
+
+            var cipher = AesCipher.Cipher(key, inputState);
+
+            Assert.AreEqual(cipher.ToString(), expectedState.ToString());
+        }
+    }
+}
